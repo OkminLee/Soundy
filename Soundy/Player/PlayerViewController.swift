3091f80165dd073e4a898968eef7274290243690
leeokmin <leeokmin@gmail.com>
2020-08-29 18:04:02 +0900

    추가 : 플레이어 정보 표시

--- Soundy/Player/PlayerViewController.swift
+++ Soundy/Player/PlayerViewController.swift
@@ -15,10 +15,58 @@ class PlayerViewController: SoundyViewController<PlayerView, PlayerViewModel> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDIdLoad")
+        initView()
+        bindViewModel()
+    }
+    
+    private func initView() {
+        guard let item = currentMusic else { return }
+        viewModel.input.requestSongTitle(item: item)
+        viewModel.input.requestArtwork(item: item, size: rootView.artworkImageView.intrinsicContentSize)
+        viewModel.input.requestAlbumTitle(item: item)
+        viewModel.input.requestArtist(item: item)
     }
 
     @IBAction func closeAction(_ sender: Any) {
         print("closeAction")
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PlayerViewController {
+    private func bindViewModel() {
+        bindSongTitle()
+        bindArtwork()
+        bindAlbumTitle()
+        bindArtist()
+    }
+    
+    private func bindSongTitle() {
+        viewModel.output.songTitle.bind { [weak self] songTitle in
+            guard let songTitle = songTitle else { return }
+            self?.rootView.songTitleLabel.text = songTitle
+        }
+    }
+    
+    private func bindArtwork() {
+        viewModel.output.artwork.bind { [weak self] artwork in
+            guard let artwork = artwork else { return }
+            self?.rootView.artworkImageView.image = artwork
+        }
+    }
+    
+    private func bindAlbumTitle() {
+        viewModel.output.albumTitle.bind { [weak self] albumTitle in
+            guard let albumTitle = albumTitle else { return }
+            self?.rootView.albumTitleLabel.text = albumTitle
+        }
+    }
+    
+    private func bindArtist() {
+        viewModel.output.artist.bind { [weak self] artist in
+            guard let artist = artist else { return }
+            self?.rootView.artistLabel.text = artist
+        }
     }
 }
--- Soundy/Player/PlayerViewModel.swift
+++ Soundy/Player/PlayerViewModel.swift
@@ -6,8 +6,49 @@
 //  Copyright © 2020 okmin lee. All rights reserved.
 //
 
-import Foundation
+import MediaPlayer
 
-class PlayerViewModel: NSObject {
+protocol PlayerViewModelInput: AlbumViewModelInput {
+    func requestSongTitle(item: MPMediaItem)
+}
+
+protocol PlayerViewModelOutput: AlbumViewModelOutput {
+    var songTitle: State<String?> { get }
+}
+
+class PlayerViewModel: NSObject, PlayerViewModelOutput {
+    var input: PlayerViewModelInput { return self }
+    var output: PlayerViewModelOutput { return self }
     
+    var songTitle = State<String?>(nil)
+    var artwork = State<UIImage?>(nil)
+    var albumTitle = State<String?>(nil)
+    var artist = State<String?>(nil)
+    var completedRequestSongs = State<Bool?>(nil)
+}
+
+extension PlayerViewModel: PlayerViewModelInput {
+    func requestSongTitle(item: MPMediaItem) {
+        songTitle.value = item.title
+    }
+    
+    func requestArtwork(item: MPMediaItem, size: CGSize) {
+        let artwork: UIImage
+        if let image = item.artwork?.image(at: size) {
+            artwork = image
+        } else {
+            artwork = UIImage().defaultMusicImage
+        }
+        self.artwork.value = artwork
+    }
+    
+    func requestAlbumTitle(item: MPMediaItem) {
+        albumTitle.value = item.albumTitle
+    }
+    
+    func requestArtist(item: MPMediaItem) {
+        artist.value = item.artist
+    }
+    
+    func requestSongs(album: MPMediaItemCollection) {}
 }
