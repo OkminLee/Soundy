46c2cf2162e4f927d8b06b87b3fcff6aef278c69
leeokmin <leeokmin@gmail.com>
2020-08-29 15:38:33 +0900

    추가 : songsTableView 넘버와 타이틀 표시하도록 추가하고 cell을 선택할 때 음악 재생하도록 함

---
+++ Soundy/Album/View/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  Soundy
+//
+//  Created by okmin lee on 2020/08/29.
+//  Copyright © 2020 okmin lee. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
